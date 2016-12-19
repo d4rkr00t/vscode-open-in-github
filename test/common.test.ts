013b832442b0d94fc80c7f080c1f8b9f9600dcfd
Stanislav Sysoev <d4rkr00t@gmail.com>
2016-12-20 10:53:33 +1100

    chore(tests): Rename some repos in mocks

--- test/common.test.ts
+++ test/common.test.ts
@@ -47,7 +47,7 @@ suite('#formatRemotes', () => {
     'ftps://host.xz/path/to/repo.git/',
     'http://host.xz/path/to/repo.git/',
     'ftp://host.xz/path/to/repo.git/',
-    'https://lillecarl@github.com/Lillecarl/mangos-tbc.git'
+    'https://user@github.com/some/repo.git'
   ];
 
   test('should correctly format all types of git remote urls', () => {
@@ -66,7 +66,7 @@ suite('#formatRemotes', () => {
         'https://host.xz/path/to/repo.git',
         'http://host.xz/path/to/repo.git',
         'http://host.xz/path/to/repo.git',
-        'https://github.com/Lillecarl/mangos-tbc',
+        'https://github.com/some/repo',
       ]
     );
   });
